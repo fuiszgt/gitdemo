63e4123a94fa0b03d3ba218962da287956ea66fe
fuiszgt <fuiszgabor@gmail.com>
2016-11-14 13:46:03 +0100

    rename to buzz

--- buzz.cpp
+++ buzz.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main()
 {
     for(int i=1;i<=100;i++){
-        if(i%3==0){
-            cout<<"Fizz"<<endl;
+        if(i%4==0){
+            cout<<"Buzz"<<endl;
         }else{
             cout<<i<<endl;
         }
