c092f03fd486ba059dbcdbe4ed5becdebe60c217
fuiszgt <fuiszgabor@gmail.com>
2016-11-14 13:39:42 +0100

    added every third is fizz

--- fizz.cpp
+++ fizz.cpp
@@ -5,7 +5,11 @@ using namespace std;
 int main()
 {
     for(int i=1;i<=100;i++){
-        cout<<i<<endl;
+        if(i%3==0){
+            cout<<"Fizz"<<endl;
+        }else{
+            cout<<i<<endl;
+        }
     }
     return 0;
 }
