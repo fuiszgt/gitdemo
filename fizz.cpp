f54157b5bd6dae2885465bc64c530a1897844bc9
fuiszgt <fuiszgabor@gmail.com>
2016-11-14 13:55:48 +0100

    readded fizz

--- fizz.cpp
+++ fizz.cpp
@@ -2,11 +2,22 @@
 
 using namespace std;
 
-int main()
-{
+int Buzz(){
     for(int i=1;i<=100;i++){
         if(i%4==0){
             cout<<"Buzz"<<endl;
+        }else{
+            cout<<i<<endl;
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    for(int i=1;i<=100;i++){
+        if(i%3==0){
+            cout<<"Fizz"<<endl;
         }else{
             cout<<i<<endl;
         }
